2e3496d6c48d134007a31086622cee1c77849306
Joseph Lally <joseph.lally@darafi.com>
2022-02-09 12:40:21 -0500

    update

--- build.js
+++ build.js
@@ -1,74 +1,67 @@
-const fs = require('fs');
-const fetch = require('isomorphic-fetch');
-const sortby = require('lodash.sortby');
-const request = require('sync-request');
-const ora = require('ora');
-const chalk = require('chalk');
+const fs = require("fs");
+const fetch = require("isomorphic-fetch");
+const sortby = require("lodash.sortby");
+const ora = require("ora");
+const chalk = require("chalk");
 
-const endpoint = 'https://www.cryptocompare.com/api/data/coinlist/';
+const endpoint = "https://www.cryptocompare.com/api/data/coinlist/";
 
-const spinner = ora('Building currencies').start();
-spinner.color = 'magenta';
+const spinner = ora("Building currencies").start();
+spinner.color = "magenta";
 
 fetch(endpoint)
-.then(response => response.json())
-.then(json => {
-	const sorted = sortby(json.Data, o => o.CoinName);
+	.then((response) => response.json())
+	.then((json) => {
+		const sorted = sortby(json.Data, (o) => o.CoinName);
 
-	const symbols = {};
-	let imagesSaved = 0;
+		const symbols = {};
 
-	/**
-	 * Build the JSON file based on the cryptocompare coinlist.
-	 */
-	sorted.forEach((currency, index) => {
-		const {Name, CoinName, ImageUrl} = currency;
-		symbols[Name] = CoinName.trim();
+		spinner.color = "yellow";
+		spinner.start(`Saving cryptocurrencies.json file`);
 
-		// download the image for future use
-		if (ImageUrl) {
-			spinner.text = `${chalk.gray(index)} ${Name}`;
-			spinner.render();
-			const res = request('get', `https://www.cryptocompare.com${ImageUrl}`);
-			fs.writeFileSync(`images/${Name.replace(/[:*?\\/<>|]/g, '_')}.${ImageUrl.split('.').pop()}`, res.getBody());
-			imagesSaved += 1;
-		}
-	});
+		fs.writeFileSync("cryptocurrencies.json", JSON.stringify(symbols, null, 2));
+		spinner.succeed(
+			`${sorted.length} currencies saved to cryptocurrencies.json`
+		);
 
-	spinner.succeed([`${imagesSaved} images saved to /images`]);
+		spinner.start("Saving Readme");
 
-	spinner.color = 'yellow';
-	spinner.start(`Saving cryptocurrencies.json file`);
+		/**
+		 * Build the Markdown Table of currencies in the Readme.
+		 */
+		const template = fs.readFileSync("readme.md").toString();
+		const data = JSON.parse(
+			fs.readFileSync("cryptocurrencies.json").toString()
+		);
 
-	fs.writeFileSync('cryptocurrencies.json', JSON.stringify(symbols, null, 2));
-	spinner.succeed(`${sorted.length} currencies saved to cryptocurrencies.json`);
+		const newSymbols = Object.keys(data);
 
-	spinner.start('Saving Readme');
+		let table = `There are currently **${newSymbols.length} cryptocurrencies** represented*:\n`;
+		table += `\n<small><em>* Last updated: ${new Date().toUTCString()}</em></small>`;
+		table += "\n\n";
+		table += "| Symbol | Name |\n";
+		table += "| :------ | :------ |\n";
 
-	/**
-	 * Build the Markdown Table of currencies in the Readme.
-	 */
-	const template = fs.readFileSync('readme.md').toString();
-	const data = JSON.parse(fs.readFileSync('cryptocurrencies.json').toString());
+		newSymbols.forEach((symbol) => {
+			table += `| \`${symbol}\` | ${data[symbol]} |\n`;
+		});
 
-	const newSymbols = Object.keys(data);
+		// Look for the HTML comments in the README as a target
+		const targetRegex =
+			/<!-- BEGIN TABLE INJECT -->(\w|\W)*<!-- END TABLE INJECT -->/gim;
+		const updated = template.replace(
+			targetRegex,
+			`<!-- BEGIN TABLE INJECT -->\n${table}\n<!-- END TABLE INJECT -->`
+		);
+		fs.writeFileSync("readme.md", updated);
+		spinner.succeed(["Readme Markdown Table updated"]);
 
-	let table = `There are currently **${newSymbols.length} cryptocurrencies** represented*:\n`;
-	table += `\n<small><em>* Last updated: ${new Date().toUTCString()}</em></small>`;
-	table += '\n\n';
-	table += '| Symbol | Name |\n';
-	table += '| :------ | :------ |\n';
-
-	newSymbols.forEach(symbol => {
-		table += `| \`${symbol}\` | ${data[symbol]} |\n`;
-	});
-
-	// Look for the HTML comments in the README as a target
-	const targetRegex = /<!-- BEGIN TABLE INJECT -->(\w|\W)*<!-- END TABLE INJECT -->/gim;
-	const updated = template.replace(targetRegex, `<!-- BEGIN TABLE INJECT -->\n${table}\n<!-- END TABLE INJECT -->`);
-	fs.writeFileSync('readme.md', updated);
-	spinner.succeed(['Readme Markdown Table updated']);
-
-	console.log('\n', 'Remember to', chalk.yellow('git commit'), 'and', chalk.yellow('npm publish'));
-})
-.catch(err => console.error(err));
+		console.log(
+			"\n",
+			"Remember to",
+			chalk.yellow("git commit"),
+			"and",
+			chalk.yellow("npm publish")
+		);
+	})
+	.catch((err) => console.error(err));
